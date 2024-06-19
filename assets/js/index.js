ac411cc0cd6c84a34640c0f69db8ef9168a9e445
Vaishak Nair <nairvaishak101@gmail.com>
2024-06-19 06:37:00 +0530

    Dates input added

---
+++ assets/js/index.js
@@ -0,0 +1,13 @@
+document.addEventListener('DOMContentLoaded', function () {
+  const pickDateInput = document.getElementById('pick-date');
+  const dropDateInput = document.getElementById('drop-date');
+
+  const today = new Date().toISOString().split('T')[0];
+  pickDateInput.setAttribute('min', today);
+  dropDateInput.setAttribute('min', today);
+
+  pickDateInput.addEventListener('change', function () {
+    const pickDate = pickDateInput.value;
+    dropDateInput.setAttribute('min', pickDate);
+  });
+});
