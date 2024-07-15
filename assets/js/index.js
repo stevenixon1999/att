6769ad51e4e948dc76b474e45bd0b5406646174f
Steve Nixon <snixon0@gmail.com>
2024-07-15 19:49:13 +0530

    Pricing Section Added And Phone Number Updated

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,4 +1,5 @@
 document.addEventListener('DOMContentLoaded', function () {
+  initializeAccordion();
   const pickDateInput = document.getElementById('pick-date');
   const dropDateInput = document.getElementById('drop-date');
 
@@ -10,4 +11,28 @@ document.addEventListener('DOMContentLoaded', function () {
     const pickDate = pickDateInput.value;
     dropDateInput.setAttribute('min', pickDate);
   });
+
+  function initializeAccordion() {
+    const accordionBtns = document.getElementsByClassName("accordion-button");
+    if (!accordionBtns.length) return;
+    for (let i = 0; i < accordionBtns.length; i++) {
+      accordionBtns[i].addEventListener("click", function () {
+        const currentActiveAcc = document.querySelector(".accordion-button.active");
+        if (currentActiveAcc !== null && currentActiveAcc !== this) {
+          const currentActiveAccItem = currentActiveAcc.closest(".accordion-item");
+          const currentActiveAccBody = currentActiveAccItem.querySelector(".accordion-body");
+          currentActiveAcc.classList.remove("active");
+          currentActiveAccBody.style.maxHeight = null;
+        }
+        this.classList.toggle("active");
+        const accordionItem = this.closest(".accordion-item");
+        const accordionBody = accordionItem.querySelector(".accordion-body");
+        accordionBody.style.maxHeight = accordionBody.style.maxHeight ? null : accordionBody.scrollHeight + "px";
+      });
+    }
+  }
+
+
+   
+
 });
